8874f36c33bc180f2eabea25a3644b5472ab46eb
Ella <ella.hyvarinen@hotmail.com>
2019-02-08 08:43:31 +0200

    child theme init

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles',99);
+function child_enqueue_styles() {
+    $parent_style = 'parent-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+}
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+       update_option( 'theme_mods_' . get_template(), $value );
+         return $old_value; // prevent update to child theme mods
+     }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
+}
+
+
+//remove Exclusive Products from ShopIsle single product page
+add_action( 'wp_loaded', function() {
+    remove_action( 'woocommerce_after_single_product', 'shop_isle_products_slider_on_single_page', 10 );
+} );
+
+?>
